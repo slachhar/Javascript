c5e0acc192485e32a9e9981e4642cd2f832091bb
Shefali Lachhar <shefali.lachhar@gmail.com>
2018-12-25 20:45:54 +0530

    Objects and constants

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ console.log(name);
 //cannot start with a number
 //cannot contain a space or hyphen
 //Are case-sensitive
+
+let interestRate = 0.3;
+interestRate = 1;
+
+let person = {
+  name: "Shefali",
+  id: 35
+};
+
+person["name"] = "lachhar";
+
+let selection = "name";
+person[selection] = "shefali lachhar";
+console.log(person.name);
